125a7d502e75d4c14eb5b8b37cb5ba152c3ba977
po <po@localhost>
2020-10-08 17:14:54 -0400

    impl: css for example

--- examples/index.php
+++ examples/index.php
@@ -7,17 +7,21 @@ function make_chart() {
     $chart = new PieChart("myChart");
     $chart->set_responsive(false);
     for ($x = rand() % 7 + 3; $x > 0; $x--){
-        $chart->add_data(new Datarow("label" . "$x", rand(), Color::rand(), Color::rand()));
+        $chart->add_data(new Datarow('label' . "$x", rand(), Color::rand(), Color::rand()));
     }
-    $chart->set_label("lb");
+    $chart->set_label('Label');
     return $chart;
 }
+echo "<!DOCTYPE html>\n";
 echo "<html>\n";
+echo "<head>\n";
+echo "<link rel='stylesheet' href='example.css' />\n";
+echo "</head>\n";
 echo "<body>\n";
-echo '<canvas id="myChart" width="400" height="400"><p>Hello Fallback World</p></canvas>' . "\n";
-echo '<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3"></script>' . "\n";
+echo "<canvas id='myChart' class='center' width='400' height='400'><p>Hello Fallback World</p></canvas>\n";
+echo "<script src='https://cdn.jsdelivr.net/npm/chart.js@2.9.3'></script>\n";
 echo make_chart();
-echo '<input type="submit" value="generate" onclick="location.reload()">';
-echo "</body>";
-echo "</html>";
+echo "<input type='submit' value='generate' class='center' onclick='location.reload()'>\n";
+echo "</body>\n";
+echo "</html>\n";
 ?>
