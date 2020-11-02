a2e2dbf6e958f1fb0a344683c58874f7b3bca980
Daher <Daher@DESKTOP-2131T6Q>
2020-11-02 16:49:25 -0500

    Added set label method to the class
    Issue #76

--- src/Charts/Bar_chart.php
+++ src/Charts/Bar_chart.php
@@ -8,15 +8,20 @@ require_once 'Charts/Bar_chart/Datarow.php';
 final class BarChart extends Chart {
     
     private ?ArrayObject $dataset;
+    private ?String $label;
     
     public function __construct(String $chart_id) {
         parent::__construct($chart_id);
         $this->dataset = new ArrayOfDataset();
     }
- 
     
-    
-    
-    
+    /**
+     * Sets the label of the bar chart
+     * @param String $label
+     */
+    public function set_label(String $label) {
+        $this->label = $label;
+    }
+       
 }
 ?>
