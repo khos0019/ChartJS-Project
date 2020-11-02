d82a8e8c461e43f9b8bde3b3aa00c06228d30f81
khos0019 <65921674+khos0019@users.noreply.github.com>
2020-11-01 22:09:26 -0500

    Added the ArrayObject varaiable and constructor.
    Issue #83

--- src/Charts/Bar_chart.php
+++ src/Charts/Bar_chart.php
@@ -5,10 +5,18 @@ require_once 'Charts/Bar_chart/Datarow.php';
 ?>
 <?php
 
-
-
-
-
-
-
+final class BarChart extends Chart {
+    
+    private ?ArrayObject $dataset;
+    
+    public function __construct(String $chart_id) {
+        parent::__construct($chart_id);
+        $this->dataset = new ArrayOfDataset();
+    }
+ 
+    
+    
+    
+    
+}
 ?>
