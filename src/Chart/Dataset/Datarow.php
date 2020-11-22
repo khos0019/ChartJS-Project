484cd099a31d329e621bc2ff5a3c2c903b810062
Daher <Daher@DESKTOP-2131T6Q>
2020-11-22 16:05:35 -0500

    Fixed get method

--- src/Chart/Dataset/Datarow.php
+++ src/Chart/Dataset/Datarow.php
@@ -1,61 +1,87 @@
 <?php
 
-abstract class AbsDatarow {
-    
+abstract class AbsDatarow
+{
+
     private array $properties = [];
+
     /**
-     *add_property set String value for Label
-     *@param String $which $value
-     *@return void
+     * add_property set String value for Label
+     *
+     * @param String $which
+     *            $value
+     * @return void
      */
-    public final function add_property(string $which, $value): void {
+    public final function add_property(string $which, $value): void
+    {
         $this->properties[$which] = $value;
     }
+
     /**
-     *with_property add two properties
-     *@param String $which $value
-     *@return $this itself
+     * with_property add two properties
+     *
+     * @param String $which
+     *            $value
+     * @return $this itself
      */
-    public final function with_property(string $which, $value): Self {
+    public final function with_property(string $which, $value): Self
+    {
         $this->add_property($which, $value);
         return $this;
     }
+
     /**
-     *return the specified properties
-     *@param String $which which properity to return
-     *@return object|Null the specificed properties
+     * return the specified properties
+     *
+     * @param String $which
+     *            which properity to return
+     * @return object|Null the specificed properties
      */
-    public final function get_property(string $which): ?object {
+    public final function get_property(string $which)
+    {
         return $this->properties[$which];
     }
+    
+//     public final function get_value($which): float
+//     {
+//         return $this->properties[$which];
+//     }
+
     /**
-     *get all properties value
-     *@return $this->properties
+     * get all properties value
+     *
+     * @return $this->properties
      */
-    public final function get_properties(): array {
+    public final function get_properties(): array
+    {
         return $this->properties;
     }
-    
+
     /**
      * Evals the given property into string format accrodingly.
-     * @param object $obj object to evaluate
+     *
+     * @param object $obj
+     *            object to evaluate
      * @return String accroding string representation of the given object
      */
-    private static function eval_properity($obj): String {
+    private static function eval_properity($obj): String
+    {
         if (is_array($obj)) {
             return "['" . implode("','", $obj) . "']";
-        }
-        else {
+        } else {
             return "$obj";
         }
     }
+
     /**
      * _toString is print out all value of properties
-     *@return String
+     *
+     * @return String
      */
-    public final function __toString(): String {
+    public final function __toString(): String
+    {
         $str = "{";
-        foreach ($this->properties as $key=>$value) {
+        foreach ($this->properties as $key => $value) {
             $str = $str . "\n" . "$key: " . Self::eval_properity($value) . ",";
         }
         $str = rtrim($str, ',');
@@ -64,13 +90,17 @@ abstract class AbsDatarow {
     }
 }
 
-final class ArrayOfDatarow extends ArrayObject {
+final class ArrayOfDatarow extends ArrayObject
+{
+
     /**
-     *Create a new datarow after eval
-     *@param $index, $newval
+     * Create a new datarow after eval
+     *
+     * @param $index, $newval
      *
      */
-    public function offsetSet($index, $newval) {
+    public function offsetSet($index, $newval)
+    {
         if ($newval instanceof AbsDatarow) {
             return parent::offsetSet($index, $newval);
         }
